414fbff69018a6e16870bc469f614105dbd08eb1
Ryan Moran <ryan.moran@gmail.com>
2011-05-31 15:44:08 -0700

    request params

--- lib/polaris_resource/request.rb
+++ lib/polaris_resource/request.rb
@@ -2,22 +2,26 @@ module PolarisResource
   class Request
     
     def self.get(path, params = {})
-      response = Typhoeus::Request.get(build_path(path), params)
+      response = Typhoeus::Request.get(build_path(path), build_params(params))
       PolarisResource::Response.new(response)
     end
 
     def self.post(path, params = {})
-      response = Typhoeus::Request.post(build_path(path), params)
+      response = Typhoeus::Request.post(build_path(path), build_params(params))
       PolarisResource::Response.new(response)
     end
 
     def self.put(path, params = {})
-      response = Typhoeus::Request.put(build_path(path), params)
+      response = Typhoeus::Request.put(build_path(path), build_params(params))
       PolarisResource::Response.new(response)
     end
     
     def self.build_path(path)
       "#{PolarisResource::Configuration.host}#{path}"
+    end
+    
+    def self.build_params(params)
+      params.empty? ? {} : { :params => params }
     end
     
   end
