839975416843e236ef7643badadda853b82cec9f
Ryan Moran <ryan.moran@gmail.com>
2011-06-02 12:47:08 -0700

    added request cache with middleware to expire the cache between requests

--- lib/polaris_client/railtie.rb
+++ lib/polaris_client/railtie.rb
@@ -1,1 +1,11 @@
-ActiveRecord::Base.send(:include, PolarisClient::Associations) if defined?(ActiveRecord)
+module PolarisClient
+  class Railtie < Rails::Railtie
+
+    ActiveRecord::Base.send(:include, PolarisClient::Associations) if defined?(ActiveRecord)
+
+    initializer "polaris_client.configure_request_cache" do |app|
+      app.middleware.use PolarisResource::RequestCache
+    end
+
+  end
+end
--- lib/polaris_resource/request.rb
+++ lib/polaris_resource/request.rb
@@ -1,8 +1,12 @@
 module PolarisResource
   class Request
-    
+
+    def self.cache
+      @cache ||= {}
+    end
+
     def self.get(path, params = {})
-      response = Typhoeus::Request.get(build_path(path), build_params(params))
+      response = cache[[path, params]] ||= Typhoeus::Request.get(build_path(path), build_params(params))
       PolarisResource::Response.new(response)
     end
 
@@ -15,14 +19,28 @@ module PolarisResource
       response = Typhoeus::Request.put(build_path(path), build_params(params))
       PolarisResource::Response.new(response)
     end
-    
+
     def self.build_path(path)
       "#{PolarisResource::Configuration.host}#{path}"
     end
-    
+
     def self.build_params(params)
       params.empty? ? {} : { :params => params }
     end
-    
+
+  end
+
+  # RequestCache middleware
+  class RequestCache
+
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      PolarisResource::Request.cache.clear
+      @app.call(env)
+    end
+
   end
 end
