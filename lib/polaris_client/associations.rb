1582c96e2893b4640ae6b1646a328ee114c3f900
Ryan Moran <ryan.moran@gmail.com>
2011-06-14 16:30:15 -0700

    client association caching

--- lib/polaris_client/associations.rb
+++ lib/polaris_client/associations.rb
@@ -19,21 +19,23 @@ module PolarisClient
 
       def has_many_resources(association)
         define_method association do
-          PolarisResource::Associations::HasManyAssociation.new(self, association)
+          instance_variable_get("@#{association}") ||
+          instance_variable_set("@#{association}", PolarisResource::Associations::HasManyAssociation.new(self, association))
         end
 
-        define_method "#{association}=" do |target|
-          PolarisResource::Associations::HasManyAssociation.new(self, association, target)
+        define_method "#{association}=" do |targets|
+          instance_variable_set("@#{association}", PolarisResource::Associations::HasManyAssociation.new(self, association, targets))
         end
       end
 
       def has_one_resource(association)
         define_method association do
-          PolarisResource::Associations::HasOneAssociation.new(self, association)
+          instance_variable_get("@#{association}") ||
+          instance_variable_set("@#{association}", PolarisResource::Associations::HasOneAssociation.new(self, association))
         end
 
         define_method "#{association}=" do |target|
-          PolarisResource::Associations::HasOneAssociation.new(self, association, target)
+          instance_variable_set("@#{association}", PolarisResource::Associations::HasOneAssociation.new(self, association, target))
         end
       end
 
